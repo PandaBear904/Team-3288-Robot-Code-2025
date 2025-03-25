c91f3491a89bfc1fdc63a32137beef79d5de40e4
Scotty Ring <sring8742@gmail.com>
2025-03-25 11:21:51 -0600

    Went back to xbox controller

--- Robot Code 2025 Name/src/main/java/frc/robot/RobotContainer.java
+++ Robot Code 2025 Name/src/main/java/frc/robot/RobotContainer.java
@@ -23,6 +23,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.commands.ClimbCommand;
@@ -46,7 +47,8 @@ public class RobotContainer {
 
     private final Telemetry logger = new Telemetry(MaxSpeed);
 
-    private final CommandJoystick joystick = new CommandJoystick(0);
+    private final CommandXboxController joystick = new CommandXboxController(0);
+
 
     public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
     private final ClimbSusbsystem climbSusbsystem = new ClimbSusbsystem();
@@ -94,27 +96,28 @@ public class RobotContainer {
         // and Y is defined as to the left according to WPILib convention.
         drivetrain.setDefaultCommand(
             drivetrain.applyRequest(() ->
-                drive.withVelocityX(-joystick.getY() * MaxSpeed)
-                     .withVelocityY(-joystick.getX() * MaxSpeed)
-                     .withRotationalRate(-joystick.getTwist() * MaxAngularRate)
+            drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
+            .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+            .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
             )
         );
         
-        joystick.button(1).whileTrue(drivetrain.applyRequest(() -> brake)); // Trigger for brake
+        joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
+        joystick.b().whileTrue(drivetrain.applyRequest(() ->
+            point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
 
-        joystick.button(2).whileTrue(drivetrain.applyRequest(() ->
-            point.withModuleDirection(new Rotation2d(-joystick.getY(), -joystick.getX()))
         ));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
-        joystick.button(7).and(joystick.button(4)).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-        joystick.button(7).and(joystick.button(3)).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-        joystick.button(8).and(joystick.button(4)).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-        joystick.button(8).and(joystick.button(3)).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+        joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+        joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+        joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+        joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+
 
         // reset the field-centric heading on left bumper press
-        joystick.button(5).onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
+        joystick.x().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
 
         drivetrain.registerTelemetry(logger::telemeterize);
         //Climb
@@ -133,7 +136,7 @@ public class RobotContainer {
 
             autoChooser.setDefaultOption("Test Auto", new SequentialCommandGroup(
                 drivetrain.runOnce(() -> drivetrain.seedFieldCentric()), // Optional: reset heading
-                //path,   // follow the path
+                path,   // follow the path
                 climb,     // Then activate the climb
                 negativeClimb
                 ));
